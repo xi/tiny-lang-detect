315458d0a09b6e790689e7e56ccb0428eca4a4c2
Tobias Bengfort <tobias.bengfort@posteo.de>
2025-05-10 20:29:51 +0200

    gen_model: allow to pass more than two languages

--- gen_model.py
+++ gen_model.py
@@ -8,28 +8,34 @@ def get_data(lang):
     return {k: v / raw['n_words'][len(k) - 1] for k, v in raw['freq'].items()}
 
 
-def get_model(lang1, lang2, n=8, ndigits=None):
-    data1 = get_data(lang1)
-    data2 = get_data(lang2)
+def abs_diff(arr):
+    return max(arr) - min(arr)
 
-    ngrams = list(set(data1.keys()) | set(data2.keys()))
+
+def get_model(*langs, n=8, ndigits=None):
+    data = {lang: get_data(lang) for lang in langs}
+
+    ngrams = set()
+    for d in data.values():
+        ngrams.update(d.keys())
+    ngrams = list(ngrams)
 
     # prioritize by biggest absolute difference
-    ngrams.sort(key=lambda k: abs(data1.get(k, 0) - data2.get(k, 0)))
+    ngrams.sort(key=lambda k: abs_diff([d.get(k, 0) for d in data.values()]))
     ngrams = ngrams[-n:]
 
     return {
         'ngrams': ngrams,
         'freq': {
-            lang1: [round(data1.get(g, 0), ndigits) for g in ngrams],
-            lang2: [round(data2.get(g, 0), ndigits) for g in ngrams],
+            lang: [round(d.get(g, 0), ndigits) for g in ngrams]
+            for lang, d in data.items()
         },
     }
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('lang', nargs=2)
+    parser.add_argument('lang', nargs='+')
     parser.add_argument('-n', type=int, default=8)
     parser.add_argument('-p', type=int, default=4)
     args = parser.parse_args()
